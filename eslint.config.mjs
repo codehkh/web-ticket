622b7b47d2ff46327fc32eb2fbb56d4a668ff281
khhong <khhong0615@gmail.com>
2024-12-20 16:42:36 +0900

    "initial commit"

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,16 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("next/core-web-vitals", "next/typescript",'airbnb', // Airbnb의 JavaScript 스타일 규칙
+    'airbnb/hooks', // Airbnb의 React hooks 스타일 규칙
+    
+    // Prettier와 충돌을 방지하려면 eslint-config-prettier를 추가
+    'plugin:prettier/recommended', // Prettier 설정
+    
+    // TypeScript용 설정
+    'plugin:@typescript-eslint/recommended') // TypeScript 관련 규칙),
+    // Airbnb 설정 추가
+    
 ];
 
 export default eslintConfig;
