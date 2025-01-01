027ee267d5707d61edacf43ffe6ca8d226dbf35f
khhong <khhong0615@gmail.com>
2025-01-02 00:04:47 +0900

    #1 [feat] 기본 버튼 컴포넌트 파일 생성 및 타입, 스타일 정의

---
+++ src/component/Button/IconButton.tsx
@@ -0,0 +1,24 @@
+import { FC, MouseEvent } from 'react'
+
+import * as Style from '@/component/Button/Button.style'
+import { IconButtonPropsType } from './Button.type'
+
+export const IconButton: FC<IconButtonPropsType> = (props) => {
+  const { icon, buttonClickCallbackF, isActive } = props
+
+  const handleButtonClick = (e: MouseEvent<HTMLButtonElement>) => {
+    buttonClickCallbackF(e)
+  }
+
+  return (
+    <Style.IconButton
+      {...props}
+      className={isActive ? 'active' : ''}
+      onClick={handleButtonClick}
+    >
+      {icon}
+    </Style.IconButton>
+  )
+}
+
+export default IconButton
