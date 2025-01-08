ca1be155bfc07ac5aebe46e68d182302003a3b2c
khhong <khhong0615@gmail.com>
2025-01-08 20:25:36 +0900

    #1/feat 프로필 영역 왼쪽 네비바에 구현 및 애니메이션 적용

--- src/component/NavigationBar/LNavigationBar.style.ts
+++ src/component/NavigationBar/LNavigationBar.style.ts
@@ -1,7 +1,7 @@
 import styled, { css } from 'styled-components'
 
-export const Container = styled.div<{ $isOepn: boolean }>`
-  width: ${(prosp) => (prosp.$isOepn ? `240px` : `80px`)};
+export const Container = styled.div<{ $isOpen: boolean }>`
+  width: ${(prosp) => (prosp.$isOpen ? `240px` : `86px`)};
   height: 100%;
   transition: width 0.5s ease;
 
@@ -11,15 +11,79 @@ export const Container = styled.div<{ $isOepn: boolean }>`
     'follow'
     'btn';
   grid-template-rows: 100px 1fr fit-content(100%);
+  grid-template-columns: 1fr;
 
   align-items: center;
   justify-content: center;
-  padding: 10px;
+  padding: 41px 16px;
   background-color: var(--gray-100);
 `
-export const Button = styled.div<{ $isOepn: boolean }>`
+
+export const ProfileDiv = styled.div`
+  grid-area: profile;
+
+  width: 100%;
+  height: 54px;
+  display: grid;
+
+  grid-template-areas:
+    'img name'
+    'img id';
+  grid-template-rows: repeat(2, fit-content(100%));
+  grid-template-columns: fit-content(100%) 1fr;
+  grid-column-gap: 11px;
+  grid-row-gap: 2px;
+
+  justify-self: left;
+`
+
+export const KorName = styled.span<{ $isOpen: boolean }>`
+  grid-area: name;
+
+  width: 100%;
+
+  font-family: Noto Sans KR;
+  font-size: 20px;
+  font-weight: 500;
+  line-height: 28.96px;
+  text-align: left;
+
+  visibility: ${(props) => !props.$isOpen && 'hidden'};
+  opacity: ${(props) => (props.$isOpen ? 1 : 0)};
+
+  transition:
+    visibility 0.2s,
+    opacity 0.2s;
+`
+
+export const Id = styled.span<{ $isOpen: boolean }>`
+  grid-area: id;
+
+  width: 100%;
+
+  font-family: Noto Sans KR;
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 20.27px;
+  text-align: left;
+
+  color: var(--gray-200);
+  visibility: ${(props) => !props.$isOpen && 'hidden'};
+  opacity: ${(props) => (props.$isOpen ? 1 : 0)};
+
+  transition:
+    visibility 0.2s,
+    opacity 0.2s;
+`
+
+export const Button = styled.div<{ $isOpen: boolean }>`
   grid-area: btn;
 
-  transform: ${(prosp) => (prosp.$isOepn ? `rotate(180deg)` : `rotate(0deg)`)};
+  width: fit-content;
+  height: fit-content;
+
+  justify-self: center;
+
+  transform: ${(prosp) => (prosp.$isOpen ? `rotate(180deg)` : `rotate(0deg)`)};
   transition: transform 0.5s;
 `
--- src/component/NavigationBar/LNavigationBar.tsx
+++ src/component/NavigationBar/LNavigationBar.tsx
@@ -1,15 +1,28 @@
 import React, { useState } from 'react'
+
 import IconButton from '@/component/Button/IconButton'
+import Profile from '@/component/Profile/Profile'
 
 import * as Style from '@/component/NavigationBar/LNavigationBar.style'
+
 import { RightIcon } from '@/assets'
+import dummyprofile from '../../../public/img/dummyprofile.png'
 
 const LNavigationBar = () => {
   const [isOpen, setIsOpen] = useState(false)
 
   return (
-    <Style.Container $isOepn={isOpen}>
-      <Style.Button $isOepn={isOpen}>
+    <Style.Container $isOpen={isOpen}>
+      <Style.ProfileDiv>
+        <Profile
+          $size="large"
+          $imgSrc={dummyprofile.src}
+          $customCSS={{ gridArea: 'img' }}
+        />
+        <Style.KorName $isOpen={isOpen}>이름</Style.KorName>
+        <Style.Id $isOpen={isOpen}>id</Style.Id>
+      </Style.ProfileDiv>
+      <Style.Button $isOpen={isOpen}>
         <IconButton
           $sizeType={'large'}
           $buttonColorType={'icon'}
